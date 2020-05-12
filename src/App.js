ec7c326c1cbc0df1e7aa77ba9ce82c95ba5a1f02
Krzysztof Kozak <hithard5@gmail.com>
2020-05-12 20:31:07 +0200

    added routing for home, form, register, login, and logout components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
 import Home from "./components/home/Home";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Logout from "./components/logout/Logout"
+import Form from "./components/form/Form";
 
 function App() {
   return (
     <HashRouter>
       <>
         <Route exact path="/" component={Home} />
+        <Route exact path="/oddaj-rzeczy" component={Form} />
+        <Route exact path="/logowanie" component={Login} />
+        <Route exact path="/rejestracja" component={Register} />
+        <Route exact path="/wylogowano" component={Logout} />
       </>
     </HashRouter>
   );
