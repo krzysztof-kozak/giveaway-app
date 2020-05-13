36937b7fd845cad25ce4f340d30699866d278514
Krzysztof Kozak <hithard5@gmail.com>
2020-05-13 20:33:16 +0200

    completed basic layout for three-columns section

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <>
       <HomeNavHeader />
+      <HomeThreeColumns />
       <HomeContact />
       <HomeSimpleSteps />
-      <HomeThreeColumns />
       <HomeWhoWeHelp />
     </>
   );
--- src/components/home/HomeThreeColumns.js
+++ src/components/home/HomeThreeColumns.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function HomeThreeColumns() {
-  return <p>HomeThreeColumns Component</p>;
+  return (
+    <>
+      <div className="three-columns-wrapper">
+        <article className="three-columns">
+          <section className="article-statistics">
+            <h2 className="article-statistics__title">10</h2>
+            <p className="article-statistics__subtitle">ODDANYCH WORKÓW</p>
+            <p className="article-statistics__text">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam
+              erat volutpat.
+            </p>
+          </section>
+
+          <section className="article-statistics">
+            <h2 className="article-statistics__title">5</h2>
+            <p className="article-statistics__subtitle">WSPARTYCH ORGANIZACJI</p>
+            <p className="article-statistics__text">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam
+              erat volutpat.
+            </p>
+          </section>
+
+          <section className="article-statistics">
+            <h2 className="article-statistics__title">7</h2>
+            <p className="article-statistics__subtitle">ZORGANIZOWANY ZBIÓREK</p>
+            <p className="article-statistics__text">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam
+              erat volutpat.
+            </p>
+          </section>
+        </article>
+      </div>
+    </>
+  );
 }
