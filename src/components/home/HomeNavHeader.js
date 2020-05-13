76d08be124ce1fcb11cfc241a6d6b14a0ceec1e0
Krzysztof Kozak <hithard5@gmail.com>
2020-05-13 14:03:54 +0200

    header styling work in progress

--- src/components/home/HomeNavHeader.js
+++ src/components/home/HomeNavHeader.js
@@ -1,34 +1,58 @@
 import React from "react";
+import heroImage from "../../assets/hero-image.png";
 
 export default function HomeNavHeader() {
   return (
     <>
-      <button>zaloguj</button>
-      <button>załóż konto</button>
-      <ul>
-        <li>
-          <a href="start"></a>
-        </li>
-        <li>
-          <a href="o co chodzi"></a>
-        </li>
-        <li>
-          <a href="o nas"></a>
-        </li>
-        <li>
-          <a href="fundacja i organizacje"></a>
-        </li>
-        <li>
-          <a href="kontakt"></a>
-        </li>
-      </ul>
-      <h1>
-        Zannij pomagać!
-        <br />
-        Oddaj niechciane rzeczy w zaufane ręce
-      </h1>
-      <button>oddaj rzeczy</button>
-      <button>zorganizuj zbiórkę</button>
+      <header className="page-header">
+        <div className="container">
+          <section className="hero">
+            <img src={heroImage} alt="giveaway items in a box" className="hero__image" />
+
+            <div className="content-wrapper">
+              <nav className="navigation">
+                <div className="login-wrapper">
+                  <a href="#" className="navigation__link">
+                    Zaloguj
+                  </a>
+                  <a href="#" className="navigation__link">
+                    Załóż konto
+                  </a>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">Start</a>
+                  </li>
+                  <li>
+                    <a href="#">O co chodzi?</a>
+                  </li>
+                  <li>
+                    <a href="#">O nas</a>
+                  </li>
+                  <li>
+                    <a href="#">Fundacha i organizacje</a>
+                  </li>
+                  <li>
+                    <a href="#">Kontakt</a>
+                  </li>
+                </ul>
+              </nav>
+
+              <section className="intro">
+                <h1 className="intro__title">
+                  Zacznij pomagać!
+                  <br />
+                  Oddaj niechciane rzeczy w zaufane ręce
+                </h1>
+                <div className="buttons-wrapper">
+                  <button>ODDAJ RZECZY</button>
+                  <button>ZORGANIZUJ ZBIÓRKĘ</button>
+                </div>
+              </section>
+            </div>
+          </section>
+        </div>
+      </header>
     </>
   );
 }
