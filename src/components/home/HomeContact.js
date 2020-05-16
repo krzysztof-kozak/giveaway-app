4ccefdd8ae8112cdf0e61531472e5387f965120c
Krzysztof Kozak <hithard5@gmail.com>
2020-05-17 00:32:58 +0200

    WIP

--- src/components/home/HomeContact.js
+++ src/components/home/HomeContact.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function HomeContact() {
-  return <p>HomeContact Component</p>
+  const messagePlaceholder =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi exercitationem doloremque mollitia qui impedit possimus libero vel voluptatibus enim iste, dolore, eos sit quia perspiciatis minus voluptatum officia officiis cum";
+  return (
+    <>
+      <section className="contact">
+        <form className="contact-form" method="post">
+          <h2>Skontaktuj się z nami</h2>
+          <label htmlFor="userName">Wpisz swoje imię</label>
+          <input type="text" name="name" id="userName" />
+
+          <label htmlFor="userEmail">Wpisz swój email</label>
+          <input type="email" name="email" id="userEmail" />
+
+          <textarea
+            name="message"
+            id="userMessage"
+            cols="30"
+            rows="10"
+            placeholder={messagePlaceholder}
+          ></textarea>
+        </form>
+      </section>
+    </>
+  );
 }
