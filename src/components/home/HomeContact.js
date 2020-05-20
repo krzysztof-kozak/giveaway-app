1095103008a00f99ca29cf5cfdc388fdb0345000
Krzysztof Kozak <hithard5@gmail.com>
2020-05-20 13:58:14 +0200

    fixed username regex

--- src/components/home/HomeContact.js
+++ src/components/home/HomeContact.js
@@ -23,7 +23,7 @@ export default function HomeContact() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const validName = /^[A-Za-z]+$/;
+    const validName = /^[A-Za-z]*$/;
     const validEmail = /\S+@\S+\.\S+/;
 
     if (!validName.test(form.name)) {
