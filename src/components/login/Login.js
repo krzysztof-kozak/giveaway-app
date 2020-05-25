f7e7b8bf91ba68744f0389c1f43c1bc8907a0dd5
Krzysztof Kozak <hithard5@gmail.com>
2020-05-25 10:21:18 +0200

    added basic user validation for login/register

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,49 +1,84 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validEmail = /\S+@\S+\.\S+/;
+
+    if (!validEmail.test(form.email)) {
+      setEmailError('Podany email jest nieprawidłowy');
+      return;
+    }
+    setEmailError('');
+
+    if (form.password.length < 6) {
+      setPasswordError('Hasło musi mieć conajmniej 6 znaków');
+      return;
+    }
+    setPasswordError('');
+  };
+
   return (
     <>
-      <nav className="navigation container">
-        <div className="login-wrapper">
-          <Link to="/logowanie" className="login-link">
+      <nav className='navigation container'>
+        <div className='login-wrapper'>
+          <Link to='/logowanie' className='login-link'>
             Zaloguj
           </Link>
-          <Link to="/rejestracja" className="login-link">
+          <Link to='/rejestracja' className='login-link'>
             Załóż konto
           </Link>
         </div>
 
-        <ul className="menu">
+        <ul className='menu'>
           <li>
-            <Link to="/" className="menu__link">
+            <Link to='/' className='menu__link'>
               Start
             </Link>
           </li>
-          <li className="menu__link">O co chodzi</li>
-          <li className="menu__link">O nas</li>
-          <li className="menu__link">Fundacje i organizjacje</li>
-          <li className="menu__link">Kontakt</li>
+          <li className='menu__link'>O co chodzi</li>
+          <li className='menu__link'>O nas</li>
+          <li className='menu__link'>Fundacje i organizjacje</li>
+          <li className='menu__link'>Kontakt</li>
         </ul>
       </nav>
 
-      <div className="user-form-container">
-        <form className="user-form">
-          <h2 className="user-form__title"> Zaloguj się</h2>
+      <div className='user-form-container'>
+        <form className='user-form' onSubmit={handleSubmit}>
+          <h2 className='user-form__title'> Zaloguj się</h2>
+          <div className='user-form__section-container'>
+            <label htmlFor='login-form-email'>Email</label>
+            <input type='email' name='email' id='login-form-email' value={form.email} onChange={handleChange} />
+            <p className='contact-form__error'>{emailError}</p>
 
-          <div className="user-form__section-container">
-            <label htmlFor="login-form-email">Email</label>
-            <input type="email" name="email" id="login-form-email" />
-
-            <label htmlFor="login-form-password">Hasło</label>
-            <input type="password" name="password" id="login-form-password" />
+            <label htmlFor='login-form-password'>Hasło</label>
+            <input type='password' name='password' id='login-form-password' value={form.password} onChange={handleChange} />
+            <p className='contact-form__error'>{passwordError}</p>
           </div>
-
-          <div className="buttons-container">
-            <Link to="/logowanie" className="user-form__button" type="submit">
+          <div className='buttons-container'>
+            <button className='user-form__button' type='submit'>
               Zaloguj
-            </Link>
-            <Link to="/rejestracja" className="user-form__button" type="submit">
+            </button>
+            <Link to='/rejestracja' className='user-form__button' type='submit'>
               Załóż konto
             </Link>
           </div>
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,51 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [repeatPasswordError, setRepeatPasswordError] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleRepeatPasswordChange = (e) => {
+    const { value } = e.target;
+    setRepeatPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validEmail = /\S+@\S+\.\S+/;
+
+    if (!validEmail.test(form.email)) {
+      setEmailError('Podany email jest nieprawidłowy');
+      return;
+    }
+    setEmailError('');
+
+    if (form.password.length < 6) {
+      setPasswordError('Hasło musi mieć conajmniej 6 znaków');
+      return;
+    }
+    if (form.password !== repeatPassword) {
+      setRepeatPasswordError('Hasło musi się zgadzać');
+      return;
+    }
+    setRepeatPasswordError('');
+  };
+
   return (
     <>
-      <nav className="navigation container">
-        <div className="login-wrapper">
-          <Link to="/logowanie" className="login-link">
+      <nav className='navigation container'>
+        <div className='login-wrapper'>
+          <Link to='/logowanie' className='login-link'>
             Zaloguj
           </Link>
-          <Link to="/rejestracja" className="login-link">
+          <Link to='/rejestracja' className='login-link'>
             Załóż konto
           </Link>
         </div>
 
-        <ul className="menu">
+        <ul className='menu'>
           <li>
-            <Link to="/" className="menu__link">
+            <Link to='/' className='menu__link'>
               Start
             </Link>
           </li>
-          <li className="menu__link">O co chodzi</li>
-          <li className="menu__link">O nas</li>
-          <li className="menu__link">Fundacje i organizjacje</li>
-          <li className="menu__link">Kontakt</li>
+          <li className='menu__link'>O co chodzi</li>
+          <li className='menu__link'>O nas</li>
+          <li className='menu__link'>Fundacje i organizjacje</li>
+          <li className='menu__link'>Kontakt</li>
         </ul>
       </nav>
 
-      <div className="user-form-container">
-        <form className="user-form">
-          <h2 className="user-form__title"> Załóż konto</h2>
+      <div className='user-form-container'>
+        <form className='user-form' onSubmit={handleSubmit}>
+          <h2 className='user-form__title'> Rejestracja</h2>
+          <div className='user-form__section-container'>
+            <label htmlFor='login-form-email'>Email</label>
+            <input type='email' name='email' id='login-form-email' value={form.email} onChange={handleChange} />
+            <p className='contact-form__error'>{emailError}</p>
 
-          <div className="user-form__section-container">
-            <label htmlFor="login-form-email">Email</label>
-            <input type="email" name="email" id="login-form-email" />
+            <label htmlFor='login-form-password'>Hasło</label>
+            <input type='password' name='password' id='login-form-password' value={form.password} onChange={handleChange} />
+            <p className='contact-form__error'>{passwordError}</p>
 
-            <label htmlFor="login-form-password">Hasło</label>
-            <input type="password" name="password" id="login-form-password" />
+            <label htmlFor='login-form-repeat-password'>Powtórz hasło</label>
+            <input type='password' name='password' id='login-form-repeat-password' value={repeatPassword} onChange={handleRepeatPasswordChange} />
+            <p className='contact-form__error'>{repeatPasswordError}</p>
           </div>
 
-          <div className="buttons-container">
-            <Link to="/logowanie" className="user-form__button" type="submit">
+          <div className='buttons-container'>
+            <Link to='/logowanie' className='user-form__button' type='submit'>
               Zaloguj
             </Link>
-            <Link to="/rejestracja" className="user-form__button" type="submit">
+            <button className='user-form__button' type='submit'>
               Załóż konto
-            </Link>
+            </button>
           </div>
         </form>
       </div>
