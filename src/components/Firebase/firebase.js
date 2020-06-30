ce746aa00531c8896322122e3b4f97c011dada54
Krzysztof Kozak <hithard5@gmail.com>
2020-06-30 14:09:35 +0200

    firebase WIP

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyCKpHDIpQGIbwpJMgj2crdHc5b-VpaooEM',
+  authDomain: 'giveaway-app-b9274.firebaseapp.com',
+  databaseURL: 'https://giveaway-app-b9274.firebaseio.com',
+  projectId: 'giveaway-app-b9274',
+  storageBucket: 'giveaway-app-b9274.appspot.com',
+  messagingSenderId: '375871552266',
+  appId: '1:375871552266:web:7a471caf04bd40ac6298f3',
+  measurementId: 'G-D86PDJDRMJ',
+};
